cbf35378ba0eae17707e9df72cdeca63c3ce55c3
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-08-26 21:26:28 +0900

    init: spring

---
+++ backend/src/main/java/com/rejs/molib/MolibApplication.java
@@ -0,0 +1,13 @@
+package com.rejs.molib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MolibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MolibApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rejs/molib/MolibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rejs.molib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MolibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
